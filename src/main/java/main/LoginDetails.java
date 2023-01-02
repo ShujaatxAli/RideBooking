8c9655f9eec84e99a7058e1b933eec2d3b1cd7b2
AmmarAhmed0 <sp21bsse0050@maju.edu.pk>
2023-01-03 01:12:22 +0500

    Minor changes in LoginDetails

--- src/main/java/main/LoginDetails.java
+++ src/main/java/main/LoginDetails.java
@@ -7,31 +7,42 @@ public class LoginDetails {
 	
 	public int getLoginInfo(String Email, String Pass){
 		
-		int i=0;
+		int j=0;
 		
 		try {
 			
 			Class.forName("com.mysql.jdbc.Driver");
+
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ridebooking", "root", "root");
 		   	
 		   	PreparedStatement pst = conn.prepareStatement("Select Cust_Email,Cust_Pass from customer where Cust_Email=? and Cust_Pass=?");
+
 		   	pst.setString(1, Email);
+
 		   	pst.setString(2, Pass);
 
-		   	ResultSet rs = pst.executeQuery();                        
-		   	if(rs.next()){
-		   		i = 1;
+		   	ResultSet op = pst.executeQuery();                        
+
+		   	if(op.next()){
+
+		   		j = 1;
+
 		   	}
 		   	else{
-		   		 i = 0;
+
+		   		 j = 0;
+
 		   	}
 		   	conn.close();
 			
-			}catch(Exception e) {
+			}
+			catch(Exception e) {
 				
 				System.out.println(e);
 			}
-		return i;
+
+		return j;
+
 	} 
 
 }
